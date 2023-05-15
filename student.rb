a041f0150b73b11cd552a31cc4cd85931a3a7b54
Ismail <ismailmunyentwari9@gmail.com>
2023-05-15 18:32:33 +0300

    Applied changes requested

--- student.rb
+++ student.rb
@@ -1,8 +1,8 @@
 require_relative 'person'
 
 class Student < Person
-  def initialize(classroom)
-    super(age, parent_permission: true, name: 'Unknown')
+  def initialize(classroom, age, parent_permission: true, name: 'Unknown')
+    super(age, parent_permission: parent_permission, name: name)
     @classroom = classroom
   end
 
