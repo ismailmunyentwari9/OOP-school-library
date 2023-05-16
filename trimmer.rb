d5daacece546642a998eb02320135da60588b6e2
Ismail <ismailmunyentwari9@gmail.com>
2023-05-16 17:17:51 +0300

    Add changes requested by code reviewer

--- trimmer.rb
+++ trimmer.rb
@@ -3,7 +3,7 @@ class TrimmerDecorator < BaseDecorator
   def correct_name
     name = @nameable.correct_name
     if name.length > 10
-      name[0, 10]
+      name[0, 9]
     else
       name
     end
