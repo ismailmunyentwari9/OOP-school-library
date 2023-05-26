e1eb1d5b5b7eb6e3b506692619df96eae34a18a2
Caren-Koroeny <carensiya5@gmail.com>
2023-05-26 04:33:49 +0300

    Added described_class helper is used instead of explicitly mentioning the class name (Book)

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,12 +1,12 @@
 require_relative '../book'
 
-RSpec.describe Book do
+describe Book do
   describe '#initialize' do
     it 'creates a new instance of Book with the given title and author' do
       title = 'Title'
       author = 'Author'
 
-      book = Book.new(title, author)
+      book = described_class.new(title, author)
 
       expect(book.title).to eq(title)
       expect(book.author).to eq(author)
